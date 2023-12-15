d6973932f00e8ab796ce9d6d7e2d8e7b15d250d7
ETi0n <148210094+ETi0n@users.noreply.github.com>
2023-12-15 23:59:17 +0900

    [level unrated] Title: 편지, Time: 0.02 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120898. 편지/편지.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(String message) {
+        char[] charArr = message.toCharArray();
+        int answer = charArr.length * 2;
+        return answer;
+    }
+}
