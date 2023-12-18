c17607b3dd48d758c6daf53add39d89fd974a74a
ETi0n <148210094+ETi0n@users.noreply.github.com>
2023-12-18 19:34:23 +0900

    Update 외계행성의 나이.java

--- 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.java
@@ -3,15 +3,18 @@ class Solution {
         char[] charArray = String.valueOf(age).toCharArray();
         
         for(int i = 0; i < charArray.length; i++){
-            charArray[i] = toEn(charArray[i]);
+            charArray[i] = toAlienLang(charArray[i]);
         }
         
+        // String answer = charArray.toString(); 
+        // 사용 시 배열의 내용이 아닌 배열의 주소를 문자열로 변환해 [C@<해시코드>와 같은 형태의 문자열을 반환된다.
+        // 추가로 String answer = String.valueOf(charArray);도 가능하다.
         String answer = new String(charArray);
         
         return answer;
     }
     
-    public char toEn(char n){
+    public char toAlienLang(char n){
         switch(n){
             case '0': return 'a';
             case '1': return 'b';
