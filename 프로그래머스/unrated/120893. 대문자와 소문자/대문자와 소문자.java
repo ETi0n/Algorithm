1eebaa2f64cd0bf83de23c18419019cac292935f
ETi0n <148210094+ETi0n@users.noreply.github.com>
2023-12-30 00:13:50 +0900

    [level unrated] Title: 대문자와 소문자, Time: 0.69 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < my_string.length(); i++){
+            if (Character.isUpperCase(my_string.charAt(i))) {
+                sb.append(Character.toLowerCase(my_string.charAt(i)));
+            } else {
+                sb.append(Character.toUpperCase(my_string.charAt(i)));
+            }
+        }
+        
+        String answer = sb.toString();
+        return answer;
+    }
+}
