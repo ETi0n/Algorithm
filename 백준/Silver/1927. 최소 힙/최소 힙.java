b738f51c231b741973935dd89319924a0db5ddb8
ETi0n <148210094+ETi0n@users.noreply.github.com>
2024-02-02 12:38:29 +0900

    [Silver II] Title: 최소 힙, Time: 1124 ms, Memory: 32288 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+
+        // 연산의 개수
+        int n = Integer.parseInt(br.readLine());
+
+        int x;
+        for(int i=0; i<n; i++){
+            x = Integer.parseInt(br.readLine());
+
+            // x가 자연수인 경우 : 배열에 x라는 값을 추가
+            if(x>0){
+                q.add(x);
+            }
+            // x가 0인 경우 : 가장 작은 값 출력 후 배열에서 제거
+            else{
+                if(!q.isEmpty()){
+                    System.out.println(q.poll());
+                }
+                else{
+                    System.out.println(0);
+                }
+            }
+        }
+        br.close();
+    }
+}
