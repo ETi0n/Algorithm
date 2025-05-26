130cb55f0dec5b845ef905c73bbfd3dbf7f95a6b
이서연 <148210094+ETi0n@users.noreply.github.com>
2025-05-26 13:53:56 +0900

    [Silver II] Title: 최소 힙, Time: 296 ms, Memory: 26968 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -1,32 +1,38 @@
-import java.util.*;
 import java.io.*;
+import java.util.PriorityQueue;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PriorityQueue<Integer> q = new PriorityQueue<>();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // 연산의 개수
-        int n = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-        int x;
-        for(int i=0; i<n; i++){
-            x = Integer.parseInt(br.readLine());
+        // 우선 순위 큐 : 매개변수에 아무 값을 넣지 않으면 기본 정렬 방법(오름차순)을 따른다.
+        // 필요 시 Comparable interface를 상속받아 compareTo 메서드를 구현해 우선 순위 지정이 가능하다.
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
 
-            // x가 자연수인 경우 : 배열에 x라는 값을 추가
-            if(x>0){
-                q.add(x);
-            }
-            // x가 0인 경우 : 가장 작은 값 출력 후 배열에서 제거
-            else{
-                if(!q.isEmpty()){
-                    System.out.println(q.poll());
-                }
-                else{
-                    System.out.println(0);
+        // 출력용 StringBuilder
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < N; i++){
+            int n = Integer.parseInt(br.readLine());
+
+            if(n != 0){ // 자연수라면 저장
+                queue.add(n);
+            } else { // 자연수가 아니라면(= 0 이라면) 출력
+                if(queue.isEmpty()){
+                    sb.append(0 + "\n");
+                } else {
+                    sb.append(queue.poll() + "\n");
                 }
             }
         }
-        br.close();
+
+
+        bw.write(String.valueOf(sb));
+        bw.flush();
+        bw.close();
     }
 }
