72620c048b796b9688142765dd551e7292bd7d38
이서연 <148210094+ETi0n@users.noreply.github.com>
2025-05-26 17:19:37 +0900

    [Silver IV] Title: 수 찾기, Time: 484 ms, Memory: 45192 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,42 +1,35 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static StringTokenizer st;
+    private static StringBuilder sb;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        // 첫 번째 줄 : 자연수 N
         int N = Integer.parseInt(br.readLine());
-        // 그 다음 줄 : N개의 정수
         st = new StringTokenizer(br.readLine());
 
-        int[] A = new int[N];
-        for(int i = 0; i < N; i++){
-            A[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(A);
+        // 중복을 걸러내고 데이터를 key로 순서와 상관없이 확인하므로, ArrayList와 LinkedList보다 빠르다
+        HashSet<String> list = new HashSet<>();
 
-        // 그 다음 줄 : 자연수 M
+        while(st.hasMoreTokens()){
+            list.add(st.nextToken());
+        }
+
         int M = Integer.parseInt(br.readLine());
-        // 그 다음 줄 : M개의 정수
         st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
 
-        int temp, num;
-        for(int i = 0; i < M; i++){
-            num = Integer.parseInt(st.nextToken());
-            temp = binarySearch(A, 0, N-1, num);
-
-            System.out.println(temp);
+        while(st.hasMoreTokens()){
+            if(list.contains(st.nextToken())) sb.append("1" + "\n");
+            else sb.append("0" + "\n");
         }
-    }
-    
-    public static int binarySearch(int[] arr, int start, int end, int n){
-        if (start > end) return 0;
-        
-        int mid = (start + end) / 2;
-        if(arr[mid] == n) return 1;
-        else if(arr[mid] > n) return binarySearch(arr, start, mid-1, n);
-        else return binarySearch(arr, mid+1, end, n);
+
+        bw.write(String.valueOf(sb));
+        bw.close();
+        br.close();
     }
 }
